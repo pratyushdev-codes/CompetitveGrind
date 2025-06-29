e19ef52108a3d1a4295f668d36a4b7e90a7951e3
Pratyush Birole  ▲ <birole.pratyush@gmail.com>
2025-06-30 02:31:31 +0530

    Add files via upload

---
+++ Binary Search/tempCodeRunnerFile.java
@@ -0,0 +1,25 @@
+import java.security.Key;
+import java.util.*;
+public class kthnotdivisblebyN {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            
+
+            if(k<n){
+                System.out.println(k);
+                continue;
+            }   
+            int ans = k*(k/(n-1));
+
+
+            if(ans%n==0){
+               ans--;
+            }
+                System.out.println(ans);
+        }
+    }
+}
