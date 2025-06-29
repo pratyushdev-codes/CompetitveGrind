062f7e19506e3d5427bb3db48ddf418657a55d98
Pratyush Birole  ▲ <birole.pratyush@gmail.com>
2025-06-29 21:09:12 +0530

    Add files via upload

--- Binary Search/IntrestingDrinks.java
+++ Binary Search/IntrestingDrinks.java
@@ -1,16 +1,32 @@
+import java.util.*;
+
 public class IntrestingDrinks {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int prices [] = new int [N];
-        for(int i=0;i<N;i++){
+        int[] prices = new int[N];
+        for (int i = 0; i < N; i++) {
             prices[i] = sc.nextInt();
         }
-        int Q = sc.nextInt();
-        for(int i=0;i<Q;i++){
-            int val = sc.nextInt();
-        }
 
-        
+        Arrays.sort(prices); // Sort prices for binary search
+
+        int Q = sc.nextInt(); // Number of days
+        while (Q-- > 0) {
+            int val = sc.nextInt(); // Money Vasiliy can spend today
+            int low = 0, high = N;
+
+            // Binary search for upper bound
+            while (low < high) {
+                int mid = (low + high) / 2;
+                if (prices[mid] <= val) {
+                    low = mid + 1;
+                } else {
+                    high = mid;
+                }
+            }
+
+            System.out.println(low); // Number of shops with price ≤ val
+        }
     }
 }
