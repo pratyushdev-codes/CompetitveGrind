9966d0d5e694df925220bd552768e296a73c34a0
Pratyush Birole  ▲ <birole.pratyush@gmail.com>
2025-07-08 18:00:21 +0530

    Add files via upload

--- Codeforces A grind/BlankSpace.java
+++ Codeforces A grind/BlankSpace.java
@@ -2,6 +2,27 @@ import java.util.Scanner;
 
 public class BlankSpace {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            int longestLength = 0;
+            int currLen = 0;
+            for (int i = 0; i < n; i++) {
+                if (a[i] == 0) {
+                    currLen++;
+                    longestLength = Math.max(longestLength, currLen);
+                } else {
+                    currLen = 0;
+                }
+            }
+
+            System.out.println(longestLength);
+        }
     }
 }
