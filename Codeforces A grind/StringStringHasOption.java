b9fdb35441124a1d896a43215146b568c803dfc9
Pratyush Birole  ▲ <birole.pratyush@gmail.com>
2025-07-11 11:15:17 +0530

    Add files via upload

---
+++ Codeforces A grind/StringStringHasOption.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class StringStringHasOption {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            // find the minimum char and find the last occurence of it in the String and placing it in frot the String, ==> 1 Operation
+            int n = sc.nextInt();
+            String str = sc.next();
+
+    }
+}
+
+}
