7c73f91b010a628c4e42e6db78a4fdfd554c423c
Pratyush Birole  ▲ <birole.pratyush@gmail.com>
2025-07-21 01:32:36 +0530

    Add files via upload

---
+++ CP31 1000 - Rated/BlackandWhite.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class BlackandWhite {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            String str = sc.next();
+            int [] prefix = new int [n+1];
+            Arrays.fill(prefix , -1);
+
+            for(int i =0; i< n ; i++){
+                char ch = str.charAt(i);
+                if(ch =='W'){
+                    prefix[i+1] = prefix[i] +1; // maintaining a prefix array which will store how many times W is occuring, 
+                }
+            }
+
+            int min_cells = Integer.MAX_VALUE;
+        }
+    }
+}
