2505de0af05b9bdf2509ea57d8cacc99c0a45600
Pratyush ▲ <birole.pratyush@gmail.com>
2025-10-01 20:56:31 +0530

    Add files via upload

---
+++ CF 800-900 Random Problem Set/MapleandMultiplication.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class MapleAndMultiplication {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();  // number of test cases
+        
+        while (t-- > 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            
+            if (a == b) {
+                System.out.println(0);
+            } else if (a % b == 0 || b % a == 0) {
+                System.out.println(1);
+            } else {
+                System.out.println(2);
+            }
+        }
+        
+        sc.close();
+    }
+}
